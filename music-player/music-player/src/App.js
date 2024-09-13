07017f3f40a7e1173fb8ef1cd43e04b5e87c21d8
meeksfr <mika.sustar@mail.utoronto.ca>
2024-09-12 23:26:53 -0400

    music app day 2

--- music-player/music-player/src/App.js
+++ music-player/music-player/src/App.js
@@ -1,9 +1,16 @@
+import React from "react";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import "./styles/app.scss"
 
+import data from "./util"
 
 function App() {
   return (
     <div className="App">
-     <h1>music player</h1>
+      
+     <Song />
+     <Player />
     </div>
   );
 }
